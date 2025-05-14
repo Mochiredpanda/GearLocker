ce33f0df5bea85a42060ac79ca2677bf1e049c84
Jiyu Mochi He <jiyuh95@gmail.com>
2025-05-13 20:58:00 -0700

    Initial Commit

---
+++ GearLocker/GearLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GearLockerApp.swift
+//  GearLocker
+//
+//  Created by Jiyu He on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct GearLockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GearLockerTests/GearLockerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GearLockerTests.swift
+//  GearLockerTests
+//
+//  Created by Jiyu He on 5/13/25.
+//
+
+import Testing
+@testable import GearLocker
+
+struct GearLockerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
